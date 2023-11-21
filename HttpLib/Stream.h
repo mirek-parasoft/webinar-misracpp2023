5f2ca4de773b1a2f5a93dc64a6ba48310e6f29d7
Miroslaw Zielinski <mirek@parasoft.com>
2023-11-21 16:16:15 +0100

    Deviations added

--- HttpLib/Stream.h
+++ HttpLib/Stream.h
@@ -1,4 +1,4 @@
-#pragma once
+#pragma once // parasoft-suppress MISRACPP2023-19_6_1-a "Accepted, see PERMIT_INTERNAL_19_6_1_a (sharepoint doc per_int_19_6_1_a.doc)"
 namespace net
 {
 	class InputStream
