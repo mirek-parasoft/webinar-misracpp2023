04d43c49c0aecca65a331cf47b40300ec36935ce
Miroslaw Zielinski <mirek@parasoft.com>
2023-11-22 13:46:33 +0100

    Demo prep

--- HttpLib/StringUtils.cpp
+++ HttpLib/StringUtils.cpp
@@ -181,18 +181,18 @@ public:
     std::stringstream bytes;
 
 private:
-    uint16_t acc;
+    uint16_t acctor = 0;
     int bits = 0;
 
 public:
     void addBits(uint8_t value, int n)
     {
-        acc = (acc << n) + value;
+        acctor = (acctor << n) + value;
         bits += n;
         if (bits >= 8)
         {
-            bytes << (char)(acc >> (bits - 8)); // parasoft-suppress MISRACPP2023-8_2_2-a "Accepted, see PERMIT_INTERNAL_8_2_2_a (sharepoint doc per_int_8_2_2_a.doc)"
-            acc &= 0xff;
+            bytes << (char)(acctor >> (bits - 8)); // parasoft-suppress MISRACPP2023-8_2_2-a "Accepted, see PERMIT_INTERNAL_8_2_2_a (sharepoint doc per_int_8_2_2_a.doc)"
+            acctor &= 0xff;
             bits -= 8;
         }
     }
