23c74eefa7ce1cf46723cc583db3e788fc3fe598
Miroslaw Zielinski <mirek@parasoft.com>
2023-11-23 15:26:18 +0100

    New FR implemented

--- HttpLib/StringUtil.cpp
+++ HttpLib/StringUtil.cpp
@@ -4,9 +4,9 @@
 #include <random>
 #include <string.h>
 
-std::string SEED = net::StringUtils::randomString(64); 
+std::string SEED = net::StringUtils::randomString(64);  // parasoft-suppress MISRACPP2023-6_7_2-a "Accepted, see PERMIT_INTERNAL_6_7_2_a (sharepoint doc per_int_6_7_2_a.doc)"
 
-std::string net::StringUtils::randomString(int length)
+std::string net::StringUtils::randomString(int length) noexcept
 {
     const std::string alphabet = "0123456789ABCDEF";
     std::random_device random_device;
@@ -19,6 +19,7 @@ std::string net::StringUtils::randomString(int length)
     return result;
 }
 
+
 std::string net::StringUtils::toLower(std::string_view stringView)
 {
     std::string str(stringView);
@@ -27,6 +28,29 @@ std::string net::StringUtils::toLower(std::string_view stringView)
 
     return std::move(str);
 }
+
+class TBitStream
+{
+public:
+    std::stringstream bytes;
+
+private:
+    uint16_t acctor = 0;
+    int bits = 0;
+
+public:
+    void addBits(uint8_t value, int n)
+    {
+        acctor = (acctor << n) + value; 
+        bits += n;
+        if (bits >= 8)
+        {
+            bytes << (char)(acctor >> (bits - 8)); // parasoft-suppress MISRACPP2023-8_2_2-a "Accepted, see PERMIT_INTERNAL_8_2_2_a (sharepoint doc per_int_8_2_2_a.doc)"
+            acctor &= 0xff;
+            bits -= 8;
+        }
+    }
+};
 
 void net::StringUtils::parseNameValuePairs(std::list<Property>& props, std::string_view str, char delim)
 {
@@ -83,29 +107,6 @@ void net::StringUtils::parseNameValuePairs(std::list<Property>& props, std::stri
         props.push_back(Property(name.str(), value.str()));
     }
 }
-
-class TBitStream
-{
-public:
-    std::stringstream bytes;
-
-private:
-    uint16_t acctor = 0;
-    int bits = 0;
-
-public:
-    void addBits(uint8_t value, int n)
-    {
-        acctor = (acctor << n) + value;
-        bits += n;
-        if (bits >= 8)
-        {
-            bytes << (char)(acctor >> (bits - 8)); // parasoft-suppress MISRACPP2023-8_2_2-a "Accepted, see PERMIT_INTERNAL_8_2_2_a (sharepoint doc per_int_8_2_2_a.doc)"
-            acctor &= 0xff;
-            bits -= 8;
-        }
-    }
-};
 
 static int hex2dec(char ch)
 {
